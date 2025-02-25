360a1bf7a59d658cd2e73e3b8900155938c44a35
Bartavius <lojanarungsiri.j@northeastern.edu>
2025-02-24 20:47:38 -0500

    topk frequent

--- ArraysAndHashing/GroupAnagrams.java
+++ ArraysAndHashing/GroupAnagrams.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-class Solution {
+class GroupAnagramsSolution {
     @SuppressWarnings("Convert2Diamond")
     public List<List<String>> groupAnagrams(String[] strs) {
         
---
+++ ArraysAndHashing/TopKFrequentElements.py
@@ -0,0 +1,33 @@
+def topKFrequent(self, nums, k: int):
+
+    # 1. count frequency 
+    # 2. put in hashset,
+
+    # sort the numbers into: 
+    #     key: frequency, elem: all the nums that has that frequency
+
+    frequencies = [[] for i in range(len(nums))]
+    count = {}
+
+    # counting the frequency
+    for n in nums:
+        if str(n) not in count:
+            count[str(n)] = 0
+        count[str(n)] += 1
+    
+    # bucket sorting
+    for key in count.keys():
+        frequencies[count[key] - 1].append(int(key))
+
+    length = len(frequencies)
+
+    # getting output
+    res = []
+    i = length - 1
+    while len(res) < k:
+        if not frequencies[i]:
+            i -= 1
+            continue
+        res.append(frequencies[i].pop())
+    
+    return res
