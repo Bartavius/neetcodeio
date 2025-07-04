2a4590b2dbca77a36a0f000f45b778d985a5a177
Bartavius <lojanarungsiri.j@northeastern.edu>
2025-07-04 17:38:31 +0700

    reimplemented two sum and passes all tests with full coverage

--- python/two_sum.py
+++ python/two_sum.py
@@ -3,28 +3,27 @@ Given an array of integers nums and an integer target, return the indices i and 
 
 You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
 
-Return the answer with the smaller index first.
+Return the answer with the smaller INDEX first.
 '''
 
-class Solution:
-    def two_sum(nums, target):
+class TwoSum:
+    def two_sum(self, nums, target):
 
-        # define l r
-        sorted_nums = []
-        for i, item in enumerate(nums):
-            sorted_nums.append((item, i))
+        # enumerate the values of nums with their index
+        sorted_list = []
+        for i,n in enumerate(nums):
+           sorted_list.append((n, i))
+        sorted_list.sort()
 
-        sorted_nums.sort()
-        l, r = 0, len(sorted_nums) - 1
-
+        # two-sum search with a sorted list
+        l, r = 0, len(sorted_list) - 1
         while l < r:
-            if sorted_nums[l][0] + sorted_nums[r][0] == target:
-                first = sorted_nums[l][1]
-                second = sorted_nums[r][1]
-                return [min(first, second), max(first, second)]
-
-            elif sorted_nums[l][0] + sorted_nums[r][0] < target:
+            res =  sorted_list[l][0] + sorted_list[r][0]
+            if res == target:
+                return sorted([sorted_list[l][1], sorted_list[r][1]]) # return the index with smallest first
+            elif res < target:
                 l += 1
             else:
                 r -= 1
-        return []
+
+        return [] # return empty array if there's no two_sum
