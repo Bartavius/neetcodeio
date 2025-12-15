c452df00556caf10762df664d0118cdd41f769e1
Bartavius <lojanarungsiri.j@northeastern.edu>
2025-12-15 12:55:17 +0700

    put pt1 in a function

--- python/advent_of_code_2025/day1.py
+++ python/advent_of_code_2025/day1.py
@@ -9,27 +9,30 @@ the number of times the dial is left pointing at
 
 """
 
-# read the input file -> pairs of (direction, distance)
-with open("./day1.txt", 'r') as file:
-    lines = [(line[0], int(line[1:].strip())) for line in file]
+def part1():
+    # read the input file -> pairs of (direction, distance)
+    with open("./day1.txt", 'r') as file:
+        lines = [(line[0], int(line[1:].strip())) for line in file]
 
-print((-11) % 5)
+    print((-11) % 5)
 
-# start at dial 50
-current = 50
-zeros = 0
+    # start at dial 50
+    current = 50
+    zeros = 0
 
-for direction, distance in lines:
+    for direction, distance in lines:
 
-    if direction == "R":
-        current = (current + distance) % 100 # bounded
+        if direction == "R":
+            current = (current + distance) % 100 # bounded
 
-    elif direction == "L":
-        current = (current - distance) % 100 # bounded
+        elif direction == "L":
+            current = (current - distance) % 100 # bounded
 
 
-    # accumulate number of zeros
-    if current == 0:
-        zeros += 1
+        # accumulate number of zeros
+        if current == 0:
+            zeros += 1
 
-print(zeros) # 962
+    print(zeros) # 962
+
+part1()
